2d50cef253dfaa3778f740e1fe159eacc579bdea
Abhishek Singh <abhishek17513@gmail.com>
2022-06-04 21:09:49 +0530

    add treesitter telescope

---
+++ lua/core/cmp.lua
@@ -0,0 +1,55 @@
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-h>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-l>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      vim_item.menu = ({
+        nvim_lua = "[LSP]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  }
+}
+
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -30,20 +30,54 @@ if not status_ok then
 end
 
 -- Have packer use a popup window
--- packer.init {
---  display = {
---    open_fn = function()
---      return require("packer.util").float { border = "rounded" }
---    end,
---  },
---}
+ packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "none" }
+    end,
+  },
+}
 
 -- Install your plugins here
 return packer.startup(function(use)
   -- Plugins go here
-  use "wbthomason/packer.nvim"     -- Have packer manage itself
-  use "nvim-lua/popup.nvim"        -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim"      -- Useful lua functions used ny lots of plugins
+  use "wbthomason/packer.nvim"             -- Have packer manage itself
+  use "nvim-lua/popup.nvim"                -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"              -- Useful lua functions used ny lots of plugins
+
+  -- Colorscheme
+  use "abshekh/skull-vim"              -- Almost monochrome, with grey and green accent
+
+  use "norcalli/nvim-colorizer.lua"        -- Colorize hex colors in terminal
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp"                   -- The completion plugin
+  use "hrsh7th/cmp-buffer"                 -- Buffer completions
+  use "hrsh7th/cmp-path"                   -- Path completions
+  use "hrsh7th/cmp-cmdline"                -- Cmdline completions
+  use "saadparwaiz1/cmp_luasnip"           -- Snippet completions
+  use "hrsh7th/cmp-nvim-lsp"               -- LSP completion
+  use "hrsh7th/cmp-nvim-lua"               -- LSP completion for neovim lsp
+
+  -- snippets
+  use "L3MON4D3/LuaSnip"                   -- Snippet engine
+  use "rafamadriz/friendly-snippets"       -- A bunch of snippets to use
+
+
+  -- LSP
+  use "neovim/nvim-lspconfig"              -- Enable LSP
+  use "williamboman/nvim-lsp-installer"    -- Simple to use language server installer
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"              -- Rainbow Parens
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
