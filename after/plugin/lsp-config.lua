2bf504ee0822855a0336b25fcbefc935ae725d2f
Abhishek Singh <abhishek17513@gmail.com>
2022-08-07 16:51:46 +0530

    changes

---
+++ after/plugin/lsp-config.lua
@@ -0,0 +1,17 @@
+local status_ok_lspconfig, lspconfig = pcall(require, "lspconfig")
+
+if not status_ok_lspconfig then
+  return
+end
+
+local opts = {
+  on_attach = require("abshekh.lsp.handlers").on_attach,
+  capabilities = require("abshekh.lsp.handlers").capabilities,
+}
+
+-- local sumneko_opts = vim.tbl_deep_extend("force",
+--   require("abshekh.lsp.settings.sumneko_lua"),
+--   opts)
+-- lspconfig.sumneko_lua.setup(sumneko_opts)
+
+lspconfig.hls.setup(opts)
